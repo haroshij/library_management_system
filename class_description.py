0b5760028d2fd26235af8b8967fb6950d644da54
Stepan Pomogaibo <haroshij@yandex.ru>
2024-07-19 18:53:25 +0300

    add docstring and annotations

--- class_description.py
+++ class_description.py
@@ -2,30 +2,36 @@ from typing import Union
 
 
 class Book:
+    """Класс для создание экземпляров (книг)
+    с указанием следующих данных: название,
+    автор, год издания, статус (выдана или в наличии)."""
 
-    id_dict = dict()
-    unused_id = list(range(10_000, 100_000))
-
-    def __init__(self, title='Название неизвестно', author='Автор неизвестен',
-                 year='Год написания неизвестен', status='в наличии'):
+    # Инициализируем экзмепляр со всеми атрибутами.
+    # ID книги считаем через статический метод класса.
+    def __init__(self,
+                 title: str = 'Название неизвестно',
+                 author: str = 'Автор неизвестен',
+                 year: Union[int, str] = 'Год написания неизвестен',
+                 status: str = 'в наличии'):
         self.title = title
         self.author = author
         self.year = year
         self.status = status
         self.id = Book.id_generetor(title, author, year)
-        Book.id_dict[self.id] = f'{title}, {author}, {year}'
 
+    # Метод для генерации ID книги.
     @staticmethod
     def id_generetor(title: str, author: str, year: Union[int, str]):
         id = 0
+
+        # ID получаем через сложение всех порядковых номеров символов
+        # в таблице ASCII.
         for letter in title + author + str(year):
             id += ord(letter)
         return id
 
 
-# Для тестирования корректности создания экземпляров их методов.
+# Для тестирования корректности создания экземпляров.
 if __name__ == '__main__':
     mumu = Book('Mu-mu', 'Turgenev', 1970)
-    mumu2 = Book('Mu-mu', 'Turgenev', 1970)
-    print(Book.id_dict)
     print(mumu.id)
